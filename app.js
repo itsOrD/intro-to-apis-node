f636f1e7ddc99dc5b7303c2aacbe67ee6737da1e
Matthew Beckerleg <ord.sc2@gmail.com>
2020-04-12 14:34:42 -0700

    added personalized .env && corrected npm start script

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const TwilioClient = require('twilio');
 const app = express();
 const port = 3000;
+
+const client = new TwilioClient()
+const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
 
 // This is a single page application and it's all rendered in public/index.html
 app.use(express.static("public"));
@@ -10,10 +14,9 @@ app.use(express.static("public"));
 app.use(bodyParser.json());
 
 app.get("/api/compliments", async (req, res) => {
-  // TODO: Get a list of messages sent from a specific number
-  const sentMessages = [];
+  const sentMessages = await client.messages.list({ from: twilioPhoneNumber })
   // TODO: Gather only the body of those messages for sending to the client
-  const compliments = [];
+  const compliments = sentMessages.forEach(msg => msg.body)
   res.json(compliments);
 });
 
@@ -21,7 +24,12 @@ app.post("/api/compliments", async (req, res) => {
   const to = req.body.to;
   const from = process.env.TWILIO_PHONE_NUMBER;
   const body = `${req.body.sender} says: ${req.body.receiver} is ${req.body.compliment}. See more compliments at ${req.headers.referer}`;
-  // TODO: Send a message
+  client.messages
+    .create({
+      from: from,
+      body: body,
+      to: to
+    })
   res.json({ success: false });
 });
 
