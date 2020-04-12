b0005a520933a60addb1e091021b3c7c316fe4c1
Matthew Beckerleg <ord.sc2@gmail.com>
2020-04-12 14:45:21 -0700

    added error handling to 'send' function and allowed for beeter dev environment with nodemon

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ app.use(bodyParser.json());
 
 app.get("/api/compliments", async (req, res) => {
   const sentMessages = await client.messages.list({ from: twilioPhoneNumber })
-  // TODO: Gather only the body of those messages for sending to the client
-  const compliments = sentMessages.forEach(msg => msg.body)
+  const compliments = sentMessages.map(message => message.body)
   res.json(compliments);
 });
 
@@ -24,13 +23,17 @@ app.post("/api/compliments", async (req, res) => {
   const to = req.body.to;
   const from = process.env.TWILIO_PHONE_NUMBER;
   const body = `${req.body.sender} says: ${req.body.receiver} is ${req.body.compliment}. See more compliments at ${req.headers.referer}`;
-  client.messages
-    .create({
-      from: from,
-      body: body,
-      to: to
-    })
-  res.json({ success: false });
+  try {
+    await client.messages
+      .create({
+        from: from,
+        body: body,
+        to: to
+      })
+  } catch(err) {
+    res.status(err.status).json({ success: false, message: err.message });
+  }
+  res.json({ success: true });
 });
 
 app.listen(port, () => console.log(`Prototype is listening on port ${port}!`));
